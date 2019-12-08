59130ac25fb066800289859dfe1eca4002af4408
Turtle <44775810+algorithm-planet@users.noreply.github.com>
2019-12-08 14:09:11 +0530

    Create DEBUG.cs

---
+++ DEBUG.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DEBUG_audio : MonoBehaviour
+{
+    [Range(0.1f, 0.5f)]
+    public float a = 0.5f;
+    [Range(1, 12)]
+    public int index = 1;
+
+    private void Update()
+    {
+        dt = 1f / AudioSettings.outputSampleRate;
+        if (Input.GetKeyDown(KeyCode.Space))
+            t = 0f;
+    }
+
+    float t = 0f,
+          dt;
+    private void OnAudioFilterRead(float[] data, int channels)
+    {
+        for(int i = 0; i < data.Length; i += 2 )
+        {
+            t += dt;
+            float f = 220f * Mathf.Pow(2, index / 12f);
+
+            data[i] = a * Mathf.Pow(64, -t) * Mathf.Sin(2 * Mathf.PI * f * t);
+            data[i + 1] = data[i];
+        }
+    }
+}
